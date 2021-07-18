ab420a68c6b5f4a057e201f84bb148aefc775912
chandresh-rgb <cdwivedi74@gmail.com>
2021-07-18 18:35:49 +0530

    First Commit

---
+++ dist/index.4cb24f4a.js
@@ -0,0 +1,2 @@
+!function(){const e=document.querySelector(".btn"),n=document.querySelector(".body");e.addEventListener("click",(function(){console.log("javascript code is running perfectly.!!"),e.textContent="JavaScript Code is running.",n.style.backgroundColor="red"})),console.log("a varibale")}();
+//# sourceMappingURL=index.4cb24f4a.js.map
